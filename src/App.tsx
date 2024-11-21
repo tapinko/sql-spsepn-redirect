04e908f17f6dee8ef718b327f3256e074a9bd2e9
tapinko <2tapusik00patrik6.com>
2024-11-21 13:14:44 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { TextField, Button, Box } from "@mui/material"
+import React, { useState } from "react"
+
+const App = () => {
+  const [ input, setInput ] = useState<string>("") 
+
+  return (
+    <form>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+        gap={2}
+      >
+        <TextField onChange={(e) => setInput(e.target.value)} id="outlined-basic" label="meno.priezvisko" variant="outlined" />
+        <Button onClick={() => handleRedirect(input)} variant="contained">Presmerovať</Button>
+      </Box>
+    </form>
+  )
+}
+
+const handleRedirect = (input: string) => {
+  window.location.href = `http://sql.spsepn.edu.sk/~${input}@spsepn.edu.sk/default.html`
+}
+
+export default App
